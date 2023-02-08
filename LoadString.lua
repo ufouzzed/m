8378ca99e3817c5810d8bc76c3a5e22a5df31a12
Maxey <94880000+ufouzzed@users.noreply.github.com>
2023-02-07 18:39:58 -0600

    Create LoadString.lua

---
+++ LoadString.lua
@@ -0,0 +1,6 @@
+--By JMaxeyy
+--Might take a little bit of time to load dont worry your game is not frozen
+ 
+ 
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ufouzzed/Githubed/main/Update"))()
